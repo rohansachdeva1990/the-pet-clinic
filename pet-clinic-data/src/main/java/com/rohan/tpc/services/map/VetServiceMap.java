46f2542d234116dd051fffb4852ae10a07f90e22
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-07-19 15:11:37 +0200

    Implemented Map Bases Service. Closes #6

---
+++ pet-clinic-data/src/main/java/com/rohan/tpc/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.rohan.tpc.services.map;
+
+import com.rohan.tpc.model.Vet;
+import com.rohan.tpc.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
