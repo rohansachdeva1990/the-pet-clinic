bd026266d0347037c8972576b72a52e0f4b9ebe2
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-07-19 14:56:28 +0200

    Refactor service interface to common crud. Closes #11

---
+++ pet-clinic-data/src/main/java/com/rohan/tpc/services/CrudService.java
@@ -0,0 +1,16 @@
+package com.rohan.tpc.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+}
--- pet-clinic-data/src/main/java/com/rohan/tpc/services/PetService.java
+++ pet-clinic-data/src/main/java/com/rohan/tpc/services/PetService.java
@@ -2,13 +2,5 @@ package com.rohan.tpc.services;
 
 import com.rohan.tpc.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
