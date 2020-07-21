24706d87338e60eb6693e56a12727941d8288a5e
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-07-21 21:16:17 +0200

    Added Pet type to bootstrap data. Closes #32

--- pet-clinic-web/src/main/java/com/rohan/tpc/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/rohan/tpc/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.rohan.tpc.bootstrap;
 
 import com.rohan.tpc.model.Owner;
+import com.rohan.tpc.model.PetType;
 import com.rohan.tpc.model.Vet;
 import com.rohan.tpc.services.OwnerService;
+import com.rohan.tpc.services.PetTypeService;
 import com.rohan.tpc.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,19 +14,28 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType dragon = new PetType();
+        dragon.setName("Dragon");
+        PetType savedDragonPetType = petTypeService.save(dragon);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Nathan");
         owner1.setLastName("Drake");
-
-        ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Ichigo");
