4b4a81b5e18c7777b5faca5d60949d63f25aea01
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-07-21 20:51:40 +0200

    Added PetType Map Service. Closes #29

---
+++ pet-clinic-data/src/main/java/com/rohan/tpc/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.rohan.tpc.services;
+
+import com.rohan.tpc.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/rohan/tpc/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package com.rohan.tpc.services.map;
+
+import com.rohan.tpc.model.PetType;
+import com.rohan.tpc.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
