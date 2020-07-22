91f3362be74b20c2ab71b9c714328e1d247d7465
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-07-22 11:47:49 +0200

    Add speciality map service impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/rohan/tpc/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.rohan.tpc.services;
+
+import com.rohan.tpc.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/rohan/tpc/services/map/SpecialityMapService.java
@@ -0,0 +1,33 @@
+package com.rohan.tpc.services.map;
+
+import com.rohan.tpc.model.Speciality;
+import com.rohan.tpc.services.SpecialitiesService;
+
+import java.util.Set;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
