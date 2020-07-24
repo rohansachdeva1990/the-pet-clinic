42bcc62b134b102d9082ad4d798bb359fe36c51e
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-07-24 14:27:02 +0200

    Created Spring Data JPA Vet Specialty Service. Closes #44

--- pet-clinic-data/src/main/java/com/rohan/tpc/services/springdatajpa/PetSDJpaService.java
+++ pet-clinic-data/src/main/java/com/rohan/tpc/services/springdatajpa/PetSDJpaService.java
@@ -19,6 +19,8 @@ public class PetSDJpaService implements PetService {
         this.petRepository = petRepository;
     }
 
+
+
     @Override
     public Set<Pet> findAll() {
         Set<Pet> pets = new HashSet<>();
---
+++ pet-clinic-data/src/main/java/com/rohan/tpc/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,49 @@
+package com.rohan.tpc.services.springdatajpa;
+
+
+import com.rohan.tpc.model.Speciality;
+import com.rohan.tpc.respositories.SpecialityRepository;
+import com.rohan.tpc.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return specialityRepository.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        specialityRepository.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
