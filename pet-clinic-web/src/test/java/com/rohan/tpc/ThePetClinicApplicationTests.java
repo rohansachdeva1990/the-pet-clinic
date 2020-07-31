f77f844eb67c6eb752c87d347b5cc56fb48b35f8
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-07-31 21:55:01 +0200

    Upgrade to JUnit 4. Closes #4

--- pet-clinic-web/src/test/java/com/rohan/tpc/ThePetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/rohan/tpc/ThePetClinicApplicationTests.java
@@ -1,13 +1,16 @@
 package com.rohan.tpc;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ThePetClinicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
